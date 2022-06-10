9640867e7514547ab1a4c48567a054e607c35451
Michael Beutler <michael.beutler@iperka.com>
2022-06-10 08:15:13 +0200

    Fixed `StackedLayout` tests. 🍩

--- src/components/StackedLayout/StackedLayout.test.tsx
+++ src/components/StackedLayout/StackedLayout.test.tsx
@@ -5,6 +5,33 @@ import StackedLayout from "./StackedLayout";
 
 describe("StackedLayout", () => {
   test("should render StackedLayout component", () => {
-    expect(expect(render(<StackedLayout label="Hello world!" />)).toBeDefined();
+    expect(
+      render(
+        <StackedLayout
+          title="Hello world!"
+          customLink={(href, children) => (
+            <div onClick={() => alert(`Navigate to ${href}...`)}>
+              {children}
+            </div>
+          )}
+          items={[
+            { label: "Dashboard", href: "#", current: true },
+            { label: "Team", href: "#", current: false },
+            { label: "Projects", href: "#", current: false },
+            { label: "Calendar", href: "#", current: false },
+          ]}
+          userMenuItems={[
+            { label: "Profile", href: "#" },
+            { label: "Settings", href: "#" },
+            { label: "Log out", href: "#" },
+          ]}
+        >
+          {" "}
+          <div className="px-4 py-6 sm:px-0">
+            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+          </div>
+        </StackedLayout>
+      )
+    ).toBeDefined();
   });
 });

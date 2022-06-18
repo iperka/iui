308597a253a48eb6c562ce2a8082c3518508ebb2
Michael Beutler <michael.beutler@iperka.com>
2022-06-18 21:09:49 +0200

    Removed unused space. 🐿

--- src/components/StackedLayout/StackedLayout.test.tsx
+++ src/components/StackedLayout/StackedLayout.test.tsx
@@ -26,7 +26,6 @@ describe("StackedLayout", () => {
             { label: "Log out", href: "#" },
           ]}
         >
-          {" "}
           <div className="px-4 py-6 sm:px-0">
             <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
           </div>
